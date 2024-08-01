c6fcf62603913d122e507c84bd2f61cb9deb4f7a
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-08-01 14:09:51 +0200

    Add 'line' div for each line of squares
    Create a div called 'line', in each horizontal line of squares created, append each square div to the line div. When each 'line' is complete, append it to the 'container' div

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ let conEl = document.querySelector(".container");
 
 //Prints Grid
 for(let i = 0; i < grid; i++){
+    const line = document.createElement("div");
     for(let j = 0; j < grid; j++){
         const div = document.createElement("div");
         div.style.backgroundColor = "red";
         div.style.height = "20px";
         div.style.width = "20px";
         div.style.margin = "10px";
-        conEl.appendChild(div);
+        line.appendChild(div);
     }
+    conEl.appendChild(line);
 }
