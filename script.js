52c9c4ed61f5c264a1ae1ab7ea11c37de6b14865
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-08-01 14:05:11 +0200

    Create Squares for Grid

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let grid = 16;
 
 //query selector
 let conEl = document.querySelector(".container");
+
+//Prints Grid
+for(let i = 0; i < grid; i++){
+    for(let j = 0; j < grid; j++){
+        const div = document.createElement("div");
+        conEl.appendChild(div);
+    }
+}
