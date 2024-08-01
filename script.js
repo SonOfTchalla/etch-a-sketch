260d1bf2c38c8c5ec9b9f776c27e79153db61dc5
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-08-01 14:38:04 +0200

    Add classes to divs created for styling
    Use classList.add() to add css style classes to our newly created elements to style them, rather than styling them inline

--- script.js
+++ script.js
@@ -7,13 +7,10 @@ let conEl = document.querySelector(".container");
 //Prints Grid
 for(let i = 0; i < grid; i++){
     const line = document.createElement("div");
-    line.style.display = "flex";
+    line.classList.add("flex");
     for(let j = 0; j < grid; j++){
         const div = document.createElement("div");
-        div.style.backgroundColor = "red";
-        div.style.height = "calc(100vh / 16)";
-        div.style.width = "calc(100vw / 16)";
-        div.style.margin = ".1vh";
+        div.classList.add("squareStyle");
         line.appendChild(div);
     }
     conEl.appendChild(line);
