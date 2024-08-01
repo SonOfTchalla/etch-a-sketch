90bc2c7f879e8da66f411785b374d39b45ac5d89
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-08-01 14:05:50 +0200

    Style Squares
    Add backgroundColor, height, width and margin to each square div

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ let conEl = document.querySelector(".container");
 for(let i = 0; i < grid; i++){
     for(let j = 0; j < grid; j++){
         const div = document.createElement("div");
+        div.style.backgroundColor = "red";
+        div.style.height = "20px";
+        div.style.width = "20px";
+        div.style.margin = "10px";
         conEl.appendChild(div);
     }
 }
