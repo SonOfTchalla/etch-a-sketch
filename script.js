67f031aecdf0b154bbce4d318bb2d829c6bf457d
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-08-02 13:47:06 +0200

    Add function to print grid

--- script.js
+++ script.js
@@ -12,16 +12,19 @@ buttonEL.addEventListener("click", () => {
 })
 
 //Prints Grid
-for(let i = 0; i < grid; i++){
-    const line = document.createElement("div");
-    line.classList.add("flex");
-    for(let j = 0; j < grid; j++){
-        const div = document.createElement("div");
-        div.classList.add("squareStyle");
-        div.addEventListener("mouseenter", () => {
-        div.classList.add("draw");});
-        line.appendChild(div);
+function drawGrid(gr){
+    for(let i = 0; i < gr; i++){
+        const line = document.createElement("div");
+        line.classList.add("flex");
+        for(let j = 0; j < gr; j++){
+            const div = document.createElement("div");
+            div.classList.add("squareStyle");
+            div.addEventListener("mouseenter", () => {
+            div.classList.add("draw");});
+            line.appendChild(div);
+        }
+        conEl.appendChild(line);
     }
-    conEl.appendChild(line);
 }
 
+drawGrid(grid)
